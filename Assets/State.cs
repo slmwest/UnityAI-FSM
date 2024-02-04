8539429593488c6808a4d442929fcbbe03b05b07
Sheldon <36916808+slmwest@users.noreply.github.com>
2024-02-04 20:53:31 +0000

    Create Idle and Patrol classes inherited from base State class

--- Assets/State.cs
+++ Assets/State.cs
@@ -16,7 +16,7 @@ public class State
         ENTER, UPDATE, EXIT
     };
 
-    public STATE name;
+    public STATE name; // this won't be defined in the base class, only in inherited classes that build on it!
     protected EVENT stage;
     protected GameObject npc;
     protected NavMeshAgent agent;
@@ -60,3 +60,86 @@ public class State
 }
 
 // better practice to do this in another file in a production project!
+public class Idle : State
+{
+    // constructor, where base keyword used to call constructor of the base class. We pass the Idle inputs to the base class constructor.
+    public Idle(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+        : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.IDLE;
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isIdle");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        // probabilistic transitions from this state or remain in this state
+        if (Random.Range(0, 100) < 10)
+        {
+            nextState = new Patrol(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+        base.Update();
+    }
+
+    public override void Exit()
+    {
+        anim.ResetTrigger("isIdle"); // clean up queue of triggers, reduce risk of buggy anims. see McAdams system
+        base.Exit();
+    }
+}
+
+public class Patrol : State
+{
+    int currentIndex = -1;
+
+    // constructor, where base keyword used to call constructor of the base class. We pass the Patrol inputs to the base class constructor.
+    public Patrol(GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+        : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.PATROL;
+        agent.speed = 2; //navMesh property
+        agent.isStopped = false; //navMesh property
+    }
+
+    public override void Enter()
+    {
+        currentIndex = 0;
+        anim.SetTrigger("isWalking");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        if (agent.remainingDistance < 1)
+        {
+            if (currentIndex >= GameEnvironment.Singleton.Checkpoints.Count - 1)
+            {
+                currentIndex = 0;
+            } else
+            {
+                currentIndex++;
+            }
+            agent.SetDestination(GameEnvironment.Singleton.Checkpoints[currentIndex].transform.position);
+        }
+
+        // probabilistic transitions from this state or remain in this state
+        if (Random.Range(0, 100) < 5) 
+        {
+            nextState = new Idle(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+
+        base.Update();
+    }
+
+    public override void Exit()
+    {
+        anim.ResetTrigger("isWalking"); // clean up queue of triggers, reduce risk of buggy anims. see McAdams system
+        base.Exit();
+    }
+}
